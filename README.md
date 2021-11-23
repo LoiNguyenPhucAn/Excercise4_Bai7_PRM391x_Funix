1c06f8ff45a1371d936592090de70fdf00465b5c
LoiNguyenPhucAn <lnphucan@gmail.com>
2021-11-23 16:49:41 +0700

    v1.0 thực hiện yêu cầu Exercise 4.1
    sử dụng intent-filter, BroadcastReceiver và MediaPlayer để thực hiện yêu cầu phát âm thanh khi xảy ra action screen on hoặc screen off

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Excercise4_Bai7_PRM391x_Funix
- Exercise 4.1 - Lắng nghe sự kiện screen on/off Exercise 4.2 - Hiện Toast Emoji ngộ nghĩnh khi có cuộc gọi đến
+ Exercise 4.1 - Lắng nghe sự kiện screen on/off
+ Exercise 4.2 - Hiện Toast Emoji ngộ nghĩnh khi có cuộc gọi đến
---
+++ app/src/main/java/com/example/screentalk/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.screentalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        try {
+            // khai báo intent-filer trong file Manifest
+            // READ MORE: https://developer.android.com/guide/topics/manifest/intent-filter-element
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+
+
+            /*Note: Apps can receive broadcasts in two ways: through manifest-declared receivers and context-registered receivers.
+            * Read more: https://developer.android.com/guide/components/broadcasts#receiving-broadcasts
+            * =============================================================================================*
+            * Dưới đây là đoạn code đăng ký BroadcastReceiver với context
+            * READ MORE: https://developer.android.com/guide/components/broadcasts#context-registered-receivers*/
+            BroadcastReceiver receiver = new BroadcastReceiver() {
+                // khi nhận được tín hiệu từ biến receiver phương thức onReceiver() sẽ thực thi
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    doScreenTalk(intent.getAction());
+                }
+            };
+            // Đây là câu lệnh đăng ký receiver cùng intent-filter
+            /* Với câu lệnh này thì khi xảy ra 1 action đã đăng ký trong biến filter
+            * thì BroadcastReceiver sẽ nhận được tín hiệu thông qua biến receiver và chuyển flow control
+            * thực thi phương thức onReceiver()*/
+            registerReceiver(receiver, filter);
+
+        }catch (Exception ignored){
+
+        }
+    }
+
+    /*trong phương thức dưới đây thực thi hành động
+    * khi screen on sẽ gọi phương thức sayHello()
+    * khi screen off sẽ gọi phương thức sayGoodbye()*/
+    private void doScreenTalk(String action) {
+        if(action.equals(Intent.ACTION_SCREEN_ON)){
+            sayHello();
+        }else if(action.equals(Intent.ACTION_SCREEN_OFF)){
+            sayGoodbye();
+        }
+    }
+
+    //Phương thức thực thi khởi chạy file âm thanh bye_bye.mp3
+    private void sayGoodbye() {
+        MediaPlayer.create(this, R.raw.bye_bye).start();
+    }
+
+    //Phương thức thực thi khởi chạy file âm thanh hello.mp3
+    private void sayHello() {
+        MediaPlayer.create(this, R.raw.hello).start();
+    }
+}
