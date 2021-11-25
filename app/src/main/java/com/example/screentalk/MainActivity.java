420e5c99ca7174bcee08388d67101ce66dcaa181
LoiNguyenPhucAn <lnphucan@gmail.com>
2021-11-25 11:14:09 +0700

    v1.2 thực hiện yêu cầu 4.2
    đoạn code này đang bị bug khi dùng lệnh PhoneStateListener() vì API 31 không còn hỗ trợ sử dụng thay vào đó phải dùng  TelephonyCallback()
    
    https://developer.android.com/reference/android/telephony/PhoneStateListener

--- app/src/main/java/com/example/screentalk/MainActivity.java
+++ app/src/main/java/com/example/screentalk/MainActivity.java
@@ -1,21 +1,44 @@
 package com.example.screentalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==101){
+            if (permissions.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                initViews();
+            }
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{
+
+                    Manifest.permission.READ_PHONE_STATE
+
+            }, 101);
+
+        }else {
+            initViews();
+        }
     }
 
     private void initViews() {
